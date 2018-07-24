71f1a5aa878bf052157dee14e0284fd0a1f4a3b3
blank15 <yudistirosapturo@gmail.com>
2018-07-24 15:53:48 +0700

    new

---
+++ Assets/Script/DeteksiSampah.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeteksiSampah : MonoBehaviour {
+
+    public string nameTag;
+    public AudioClip audioBenar;
+    public AudioClip audioSalah;
+    private AudioSource MediaPlayerBenar;
+    private AudioSource MediaPlayerSalah;
+    public Text textScore;
+
+    // Use this for initialization
+    void Start () {
+        MediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+        MediaPlayerBenar.clip = audioBenar;
+
+        MediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+        MediaPlayerSalah.clip = audioSalah;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals(nameTag))
+        {
+            Data.score += 25;
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerBenar.Play();
+        }
+        else
+        {
+            if (Data.score > 0) {
+                Data.score -= 5;
+            }
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerSalah.Play();
+        }
+        if (Data.score <= 0)
+        {
+            Data.score = 0;
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+    }
+}
